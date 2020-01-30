f10113971cba1a2f5a436c9e9b16ad846745e4b4
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-01-30 23:27:51 +0000

    add flyway migration support

---
+++ src/main/resources/db/migration/V1__Create__user__table.sql
@@ -0,0 +1,11 @@
+-- auto-generated definition
+create table USER_1
+(
+    ID         INT,
+    ACCOUNT_ID VARCHAR(100),
+    NAME       VARCHAR(50),
+    TOKEN      CHARACTER(36),
+    GMT_CREATE BIGINT,
+    GMT_MODIFY BIGINT,
+    BIO        VARCHAR(256)
+);
