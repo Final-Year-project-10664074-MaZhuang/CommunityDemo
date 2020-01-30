9fbd951c1e464222003308ec0b999acecf2b1df6
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-01-30 21:28:55 +0000

    add H2 database

---
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package life.majiang.community.mapper;
+
+import life.majiang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into USER_1 (name,account_id,token,gmt_create,gmt_modify) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModify})")
+    void insert(User user);
+}
