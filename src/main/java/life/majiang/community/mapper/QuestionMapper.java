8d385f257e0d650f2c2284f97409a352d1123e18
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-02-01 12:39:30 +0000

    fix login bug

--- src/main/java/life/majiang/community/mapper/QuestionMapper.java
+++ src/main/java/life/majiang/community/mapper/QuestionMapper.java
@@ -5,7 +5,6 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -24,4 +23,6 @@ public interface QuestionMapper {
     List<Question> listByUserId(@Param("userId") Integer userId, @Param(value = "offset") Integer offset, @Param(value = "size")Integer size);
     @Select("select count(1) from question where creator = #{userId}")
     Integer countByUserId(@Param("userId") Integer userId);
+    @Select("select * from question where id = #{id}")
+    Question getById(@Param("id")Integer id);
 }
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -1,10 +1,7 @@
 package life.majiang.community.mapper;
 
 import life.majiang.community.model.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface UserMapper {
@@ -15,4 +12,8 @@ public interface UserMapper {
     User findByToken(@Param("token") String token);
     @Select("select * from user where account_id = #{id}")
     User findById(@Param("id") Integer id);
+    @Select("select * from user where account_id = #{accountId}")
+    User findByAccountId(@Param("accountId") String accountId);
+    @Update("update user set name = '${name}', token = '${token}', gmt_modify = '${gmtModify}', avatar_url = '${avatarUrl}' where account_id = '${accountId}'")
+    void update(User user);
 }
