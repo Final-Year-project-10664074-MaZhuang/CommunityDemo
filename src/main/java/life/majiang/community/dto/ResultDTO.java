d2f28115f88f97ac3cf66b35b990f9e52e34cb7f
SOFT356-Genshushu <zhuang.ma@students.plymouth.ac.uk>
2020-02-06 01:03:32 +0000

    add reply review function

--- src/main/java/life/majiang/community/dto/ResultDTO.java
+++ src/main/java/life/majiang/community/dto/ResultDTO.java
@@ -5,9 +5,10 @@ import life.majiang.community.exception.CustomizeException;
 import lombok.Data;
 
 @Data
-public class ResultDTO {
+public class ResultDTO<T> {
     private Integer code;
     private String message;
+    private T data;
 
     public static ResultDTO errorOf(Integer code,String message){
         ResultDTO resultDTO = new ResultDTO();
@@ -24,6 +25,14 @@ public class ResultDTO {
         ResultDTO resultDTO = new ResultDTO();
         resultDTO.setCode(200);
         resultDTO.setMessage("REQUEST SUCCESS!!!");
+        return resultDTO;
+    }
+
+    public static <T>ResultDTO okOf(T t) {
+        ResultDTO resultDTO = new ResultDTO();
+        resultDTO.setCode(200);
+        resultDTO.setMessage("REQUEST SUCCESS!!!");
+        resultDTO.setData(t);
         return resultDTO;
     }
 
--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,8 +1,11 @@
 function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
+    comment2target(questionId, 1, content);
+}
 
-    if(!content){
+function comment2target(targetId, type, content) {
+    if (!content) {
         alert("Comment content cannot be empty");
         return;
     }
@@ -12,12 +15,13 @@ function post() {
         url: "/comment",
         contentType: "application/json",
         data: JSON.stringify({
-            "parentId": questionId,
+            "parentId": targetId,
             "content": content,
-            "type": 1
+            "type": type
         }),
         success: function (response) {
             if (response.code == 200) {
+                console.log(response);
                 //$("#comment_section").hide();
                 window.location.reload();
             } else {
@@ -35,5 +39,70 @@ function post() {
             }
         },
         dataType: "json"
-    })
+    });
+}
+
+function comment(e) {
+    var commentId = e.getAttribute("data-id");
+    var content = $("#input-" + commentId).val();
+    comment2target(commentId, 2, content)
+}
+
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-" + id);
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse) {
+        console.log("collapse="+collapse);
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    } else {
+        var subCommentContainer = $("#comment-" + id);
+        console.log("subCommentContainer="+subCommentContainer.children().length);
+        if (subCommentContainer.children().length != 1) {
+            comments.addClass("in");
+            e.setAttribute("data-collapse", "in");
+            e.classList.add("active");
+        } else {
+            $.getJSON("/comment/" + id, function (data) {
+                $.each(data.data.reverse(), function (index, comment) {
+
+                    var mediaLeftElement = $("<div/>", {
+                        "class": "media-left"
+                    }).append($("<img/>", {
+                        "class": "media-object img-rounded",
+                        "src": comment.user.avatarUrl
+                    }));
+
+                    var mediaBodyElement = $("<div/>", {
+                        "class": "media-body"
+                    }).append($("<h5/>", {
+                        "class": "media-heading",
+                        "html": comment.user.name
+                    })).append($("<div/>", {
+                        "html": comment.content
+                    })).append($("<div/>", {
+                        "class": "menu"
+                    }).append($("<span/>", {
+                        "class": "pull-right",
+                        "html": moment(comment.gmtCreate).format('MM/DD, YYYY (HH:mm)')
+                    })));
+
+                    var mediaElement = $("<div/>", {
+                        "class": "media"
+                    }).append(mediaLeftElement)
+                        .append(mediaBodyElement);
+
+                    var commentElement = $("<div/>", {
+                        "class": "col-lg-12 col-md-12 col-sm-12 col-xs-12 comments"
+                    }).append(mediaElement);
+                    subCommentContainer.prepend(commentElement);
+                });
+                comments.addClass("in");
+                e.setAttribute("data-collapse", "in");
+                e.classList.add("active");
+            });
+        }
+    }
 }
